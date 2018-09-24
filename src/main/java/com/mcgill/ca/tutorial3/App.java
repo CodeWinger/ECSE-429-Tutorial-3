3420d1957a53ce8f39b837afd055fbe8d4206896
CodeWinger <mathieu.boucher5@gmail.com>
2018-09-24 16:10:18 -0400

    Fixed Unused Variable and Added Tests

--- src/main/java/com/mcgill/ca/tutorial3/App.java
+++ src/main/java/com/mcgill/ca/tutorial3/App.java
@@ -1,13 +1,17 @@
 package com.mcgill.ca.tutorial3;
 
+
 public class App
 {
-    private int x;
-
     public static void main( String[] args )
     {
-    	String s = null;
-    	System.out.println(s);
-    }
+    	if (args == null || args.length == 0) {
+    		System.out.println("no input");
+    		return;
+    	}
 
+    	for (String argument : args) {
+    		System.out.println(argument);
+    	}
+    }
 }
