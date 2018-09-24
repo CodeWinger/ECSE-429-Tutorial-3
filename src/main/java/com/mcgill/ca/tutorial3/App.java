4099c22532ac1bc28b343e7ce08a43a2d6bc242e
CodeWinger <mathieu.boucher5@gmail.com>
2018-09-24 14:59:44 -0400

    First Commit

---
+++ src/main/java/com/mcgill/ca/tutorial3/App.java
@@ -0,0 +1,13 @@
+package com.mcgill.ca.tutorial3;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
